4d915aa195a84cb1dbadddb38bd6ddd5d1f8070d
wuhao4 <wuhao4@100tal.com>
2019-10-16 16:07:10 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -21,6 +21,45 @@ class Movie(db.Model):  # 表名将会是 movie
     title = db.Column(db.String(60))    # 电影标题
     year = db.Column(db.String(4))  # 电影年份
 
+@app.route('/')
+def index():
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', movies=movies)
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % name
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例:
+    print(url_for('hello')) # 输出: /
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli')) # 输出: /user/greyli
+    print(url_for('user_page', name='peter'))  # 输出: /user/peter
+    print(url_for('test_url_for'))  # 输出: /test
+    # 下面这个调用传入了多余的关键字参数, 它们会被作为查询字符串附加到 URL 后面.
+    print(url_for('test_url_for', num=2))  # 输出: /test?num=2
+    return 'Test page'
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_founnt(e): # 接受异常对象作为参数
+    return render_template('404.html'), 404 # 返回模版和状态码
+
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典, 等同于return {'user': user}
+
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')    # 设置选项
+def initdb(drop):
+    """Initialize the datebase."""
+    if drop:    # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.') # 输出提示信息
+
 @app.cli.command()
 def forge():
     """Generate fake data."""
@@ -49,39 +88,3 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
-
-
-@app.route('/')
-#def hello():
-#    return 'Hello'
-def index():
-    user = User.query.first()   # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
-
-
-@app.route('/user/<name>')
-def user_page(name):
-    return 'User: %s' % name
-
-@app.route('/test')
-def test_url_for():
-    # 下面是一些调用示例:
-    print(url_for('hello')) # 输出: /
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for('user_page', name='greyli')) # 输出: /user/greyli
-    print(url_for('user_page', name='peter'))  # 输出: /user/peter
-    print(url_for('test_url_for'))  # 输出: /test
-    # 下面这个调用传入了多余的关键字参数, 它们会被作为查询字符串附加到 URL 后面.
-    print(url_for('test_url_for', num=2))  # 输出: /test?num=2
-    return 'Test page'
-
-@app.cli.command()  # 注册为命令
-@click.option('--drop', is_flag=True, help='Create after drop.')    # 设置选项
-def initdb(drop):
-    """Initialize the datebase."""
-    if drop:    # 判断是否输入了选项
-        db.drop_all()
-    db.create_all()
-    click.echo('Initialized database.') # 输出提示信息
-
